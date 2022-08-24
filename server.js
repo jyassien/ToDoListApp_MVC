d300eedaeff1b7074eed9c68176680922765683f
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-08-23 23:32:42 -0600

    Server.js successfully connected with indes.ejs inside views directery.

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 app.listen(process.env.PORT || 2022, () => {
   console.log("Server is running on port: ", process.env.PORT || 2022);
 });
