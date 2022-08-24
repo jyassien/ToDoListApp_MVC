97c46b46c34783a5c33c17a9a35d05e68d4a104a
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-08-23 23:42:55 -0600

    Additional roter layer added to the structure. Home page is rendered through routers that knows which controller handles the request.

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+// Routes
+const homeRoutes = require("./routes/home");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+// app.get("/", (req, res) => {
+//   res.render("index.ejs");
+// });
+app.use("/", homeRoutes);
+
 app.listen(process.env.PORT || 2022, () => {
   console.log("Server is running on port: ", process.env.PORT || 2022);
 });
