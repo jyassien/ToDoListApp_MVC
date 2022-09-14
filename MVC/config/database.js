2d85188ad234648442d5a8a4349bd7c05fe7432c
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-09-13 22:59:19 -0600

    Update the MVC todo app with authentication

---
+++ MVC/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(process.env.MONGDB_STR, {
+      useNewUrlParser: true, // This two properties are no longer supported options.
+      useUnifiedTopology: true, //      (they are not necessary)
+    });
+
+    console.log(`MongoDB Connected: ${con.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ MVC/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+// require('dotenv').config({path: './config/.env'})
+const express = require("express");
+const app = express();
+// Routes
+const homeRoutes = require("./routes/home");
+const todoRoutes = require("./routes/todos");
+const connectDB = require("./config/database");
+connectDB();
+
+app.set("view engine", "ejs");
+app.use(express.static("public")); // 'public' folder hosts static files.
+app.use(express.urlencoded({ extended: true })); // parses the request body.
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.render("index.ejs");
+// });
+
+// Separating different rotes.
+app.use("/", homeRoutes);
+app.use("/todos", todoRoutes);
+
+app.listen(process.env.PORT || 2022, () => {
+  console.log("Server is running on port: ", process.env.PORT || 2022);
+});
