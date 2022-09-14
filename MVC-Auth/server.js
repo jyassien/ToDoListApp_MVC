677701cdc872ba2203d43b668af3eed590f0e119
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-09-14 16:56:17 -0600

    Authentication midleware and session added to the server.js

--- MVC-Auth/server.js
+++ MVC-Auth/server.js
@@ -1,26 +1,47 @@
-require("dotenv").config();
-// require('dotenv').config({path: './config/.env'})
 const express = require("express");
 const app = express();
-// Routes
-const homeRoutes = require("./routes/home");
-const todoRoutes = require("./routes/todos");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
 const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const todoRoutes = require("./routes/todos");
+
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+require("./config/passport")(passport);
+
 connectDB();
 
 app.set("view engine", "ejs");
-app.use(express.static("public")); // 'public' folder hosts static files.
-app.use(express.urlencoded({ extended: true })); // parses the request body.
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
 
-// app.get("/", (req, res) => {
-//   res.render("index.ejs");
-// });
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-// Separating different rotes.
-app.use("/", homeRoutes);
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("/", mainRoutes);
 app.use("/todos", todoRoutes);
 
-app.listen(process.env.PORT || 2022, () => {
-  console.log("Server is running on port: ", process.env.PORT || 2022);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
 });
