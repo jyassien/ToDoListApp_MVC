42e4661aa066690ff1c45e5a7e24c1534831cee4
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-09-14 16:41:53 -0600

    mongodb model schema added to include user ID.

--- MVC-Auth/config/database.js
+++ MVC-Auth/config/database.js
@@ -5,9 +5,11 @@ const connectDB = async () => {
     const con = await mongoose.connect(process.env.MONGDB_STR, {
       useNewUrlParser: true, // This two properties are no longer supported options.
       useUnifiedTopology: true, //      (they are not necessary)
+      useFindAndModify: false,
+      useCreateIndex: true,
     });
 
-    // console.log(`MongoDB Connected: ${con.connection.host}`);
+    console.log(`MongoDB Connected: ${con.connection.host}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
